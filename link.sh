3746f869abb278c73e0ea49a17315cb86232abac
韦旭红 <weixuhong@shouqianba.com>
2022-01-08 13:50:41 +0800

    add gitconfig

--- link.sh
+++ link.sh
@@ -5,7 +5,7 @@
 # rm ~/.npmrc
 # ln -s ~/dotfiles/npmrc ~/.npmrc
 
-dotfiles=("zshrc" "npmrc" "SpaceVim.d" "tmux.conf" "ssh")
+dotfiles=("gitconfig" "zshrc" "npmrc" "SpaceVim.d" "tmux.conf" "ssh")
 
 for dotfile in "${dotfiles[@]}"; do
   source="$HOME/dotfiles/$dotfile"
