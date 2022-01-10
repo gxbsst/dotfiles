626ac4b8f4111167ba9e11f85384f7652e67103d
韦旭红 <weixuhong@shouqianba.com>
2022-01-10 10:21:48 +0800

    update

--- link.sh
+++ link.sh
@@ -5,6 +5,7 @@
 # rm ~/.npmrc
 # ln -s ~/dotfiles/npmrc ~/.npmrc
 
+# 需要做软链的文件或文件夹
 dotfiles=("zsh" "gitconfig" "zshrc" "npmrc" "SpaceVim.d" "tmux.conf" "ssh")
 
 for dotfile in "${dotfiles[@]}"; do
@@ -24,9 +25,14 @@ for dotfile in "${dotfiles[@]}"; do
   chmod 600 $HOME/.ssh/id_rsa
 done
 
-if [[ -e "$HOME/.config/kitty" ]]; then
-  rm -rvf $HOME/.config/kitty
+# 需要软链的.config下文件夹
+configs=("kitty")
+for config in "${configs[@]}"; do
+  
+if [[ -e "$HOME/.config/$config" ]]; then
+  rm -rvf $HOME/.config$config/
 fi
 
-ln -s $HOME/dotfiles/kitty $HOME/.config/kitty 
+ln -s $HOME/dotfiles/$config $HOME/.config/$config
 
+done
