a40a2e7a1f789cd29f730b29f20e4603f4ab2623
Weston Wei <gxbsst@gmail.com>
2022-12-16 16:11:30 +0800

    update

--- laracasts-dotfiles/nvim/lua/user/plugins.lua
+++ laracasts-dotfiles/nvim/lua/user/plugins.lua
@@ -401,10 +401,12 @@ use {
 
 use { 'neoclide/coc.nvim', branch = 'release' }
 use {'fannheyward/telescope-coc.nvim'}
-  --
+  -- Weapp
 use {'chemzqm/wxapp.vim'}
-
+-- TEXT Align
 use({'junegunn/vim-easy-align'})
+-- Mark
+use({'kshenoy/vim-signature'})
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
