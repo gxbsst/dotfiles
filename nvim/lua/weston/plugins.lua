5b6f00e619c69caec77a6db3d8b1350cb9c6faab
Weston Wei <gxbsst@gmail.com>
2022-12-26 23:09:01 +0800

    update

--- nvim/lua/weston/plugins.lua
+++ nvim/lua/weston/plugins.lua
@@ -317,3 +317,42 @@ use({
 -- Window Zoom
 --  use <C-w>m
 use({'dhruvasagar/vim-zoom'})
+
+-- Debugger
+use ({'mfussenegger/nvim-dap', config = function() 
+ local dap = require('dap')
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/dev/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require'dap.utils'.pick_process,
+  },
+}
+end
+})
+use({'theHamsta/nvim-dap-virtual-text', 
+config = function() 
+    require("nvim-dap-virtual-text").setup()
+end })
+
+use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"}, config = function() 
+    require("dapui").setup({})
+end }
+
