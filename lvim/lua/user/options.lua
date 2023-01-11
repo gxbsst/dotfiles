e0dfec3b7666f7e6feced8a928cb68aef55c07a5
Weston Wei <gxbsst@gmail.com>
2023-01-11 21:39:03 +0800

    update

--- lvim/lua/user/options.lua
+++ lvim/lua/user/options.lua
@@ -1,3 +1,49 @@
+-- local kind_icons = {
+--   Text = "",
+--   Method = "m",
+--   Function = "",
+--   Constructor = "",
+--   Field = "",
+--   Variable = "",
+--   Class = "",
+--   Interface = "",
+--   Module = "",
+--   Property = "",
+--   Unit = "",
+--   Value = "",
+--   Enum = "",
+--   Keyword = "",
+--   Snippet = "",
+--   Color = "",
+--   File = "",
+--   Reference = "",
+--   Folder = "",
+--   EnumMember = "",
+--   Constant = "",
+--   Struct = "",
+--   Event = "",
+--   Operator = "",
+--   TypeParameter = "",
+-- }
+-- lvim.builtin.cmp.formatting = {
+--   formatting = {
+--     -- fields = { "kind", "abbr", "menu" },
+--     format = function(entry, vim_item)
+--       -- Kind icons
+--       -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+--       vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+--       vim_item.menu = ({
+--         nvim_lsp = "[LSP]",
+--         luasnip = "[Snippet]",
+--         buffer = "[Buffer]",
+--         path = "[Path]",
+--       })[entry.source.name]
+--       return vim_item
+--     end,
+--   },
+-- }
+
+
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 -- vim.opt.relativenumber = true
