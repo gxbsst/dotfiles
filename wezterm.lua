de73b5a730aa19f26031f41c8c80f528fe436f57
Weston Wei <gxbsst@gmail.com>
2023-04-04 20:45:06 +0800

    update

--- wezterm.lua
+++ wezterm.lua
@@ -31,26 +31,21 @@ local colors = {
     active_tab = {
       bg_color = '#15493D',
       fg_color = '#c0c0c0',
-
       -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
       -- label shown for this tab.
       -- The default is "Normal"
       intensity = 'Normal',
-
       -- Specify whether you want "None", "Single" or "Double" underline for
       -- label shown for this tab.
       -- The default is "None"
       underline = 'None',
-
       -- Specify whether you want the text to be italic (true) or not (false)
       -- for this tab.  The default is false.
       italic = false,
-
       -- Specify whether you want the text to be rendered with strikethrough (true)
       -- or not for this tab.  The default is false.
       strikethrough = false,
     },
-
     -- Inactive tabs are the tabs that do not have focus
     inactive_tab = {
       bg_color = '#1b1032',
@@ -74,14 +69,12 @@ local colors = {
       -- The same options that were listed under the `active_tab` section above
       -- can also be used for `new_tab`.
     },
-
     -- You can configure some alternate styling when the mouse pointer
     -- moves over the new tab button
     new_tab_hover = {
       bg_color = '#3b3052',
       fg_color = '#909090',
       italic = true,
-
       -- The same options that were listed under the `active_tab` section above
       -- can also be used for `new_tab_hover`.
     },
@@ -156,7 +149,7 @@ local keys = {
 
 local font = wezterm.font_with_fallback {
   {
-    family = 'JetBrains Mono',
+    family            = 'menlo',
     -- family = 'Menlo',
     -- family = 'Fira Mono for Powerline',
     -- family = 'FiraCode Nerd Font Mono',
