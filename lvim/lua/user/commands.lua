1162a7600d91bb9e695c9de986b4f0d7d69e8633
Weston Wei <gxbsst@gmail.com>
2023-05-09 12:21:40 +0800

    实现sendToTmux方法且可正常运行

--- lvim/lua/user/commands.lua
+++ lvim/lua/user/commands.lua
@@ -1,18 +1,45 @@
 -- 给panel发送指令
 
-vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = { "" },
-  callback = function()
-    -- tmux send-keys -t 4 'wx build-npm' c-m 
-    local function send_to_tmux(target_pane, text)
-      -- 将文本转义为适合tmux使用的格式
-      local escaped_text = text:gsub("'", "'\\''")
-      -- 构造tmux发送命令
-      local cmd = string.format("tmux send-keys -t %s '%s' Enter", target_pane, text)
-      -- 执行命令
-       os.execute(cmd)
+-- Helper function to handle the arguments
+local function handle_args(args)
+    local merged_args = {}
+    local temp_arg = nil
+
+    for _, arg in ipairs(args) do
+        if temp_arg then
+            temp_arg = temp_arg .. ' ' .. arg
+            if string.sub(arg, -1) == '"' then
+                table.insert(merged_args, string.sub(temp_arg, 2, -2))
+                temp_arg = nil
+            end
+        elseif string.sub(arg, 1, 1) == '"' then
+            temp_arg = arg
+        else
+            table.insert(merged_args, arg)
+        end
+    end
+    return merged_args
+end
+
+-- Function to send command to tmux
+local function send_to_tmux(...)
+    local args = {...}
+
+    if #args == 0 then
+        print("Usage: SendToTmux <panel_id> \"'wx build-npm'\"")
+        return
     end
 
-    vim.cmd("command! -nargs=+ SendToTmux lua send_to_tmux(<f-args>)")
-  end,
-})
+    local merged_args = handle_args(args)
+    local merged_args_str = table.concat(merged_args, " ")
+    local command = string.format("tmux send-keys -t %s c-m", merged_args_str)
+    os.execute(command)
+end
+
+-- Autocommand creation
+vim.api.nvim_exec([[
+    augroup SendToTmux
+    autocmd!
+    autocmd User * command! -nargs=* SendToTmux lua send_to_tmux(<f-args>)
+    augroup END
+]], false)
