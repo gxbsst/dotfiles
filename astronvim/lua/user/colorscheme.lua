183ad19628c109c27c7673be99267f8fc2a0a50b
Weston Wei <gxbsst@gmail.com>
2023-06-27 15:01:55 +0800

    update

--- astronvim/lua/user/colorscheme.lua
+++ astronvim/lua/user/colorscheme.lua
@@ -1,4 +1,4 @@
 -- return 'kanagawa'
--- return 'tokyonight'
-return 'onedark'
+return 'tokyonight'
+-- return 'onedark'
 --------return 'rose-pine'
--- astronvim/lua/user/init.lua
+++ astronvim/lua/user/init.lua
@@ -1,8 +1,8 @@
--- vim.api.nvim_create_autocmd("ColorScheme", {
--- 	callback = function()
--- 		_G.THEME = require("user.util.theme").setup()
--- 	end
--- })
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = function()
+		_G.THEME = require("user.util.theme").setup()
+	end
+})
 
 return {
 	-- Auto Format
