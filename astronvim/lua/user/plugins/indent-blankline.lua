387ff051eb3d00370ddcdd23dca6ebcb50ce40bc
Weston Wei <gxbsst@gmail.com>
2023-07-08 00:33:53 +0800

    update

--- astronvim/lua/user/plugins/indent-blankline.lua
+++ astronvim/lua/user/plugins/indent-blankline.lua
@@ -1,6 +1,9 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   opts = function(_, opts)
+    opts.space_char_blankline = " "
+    opts.show_current_context = true
+    opts.show_current_context_start = true
     opts.show_trailing_blankline_indent = true
     return opts
   end
