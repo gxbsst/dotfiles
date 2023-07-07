f9d218d6f9be21ca6626a9a5a352f54326a2912a
Weston Wei <gxbsst@gmail.com>
2023-07-07 13:34:39 +0800

    update

--- astronvim/lua/user/plugins/indent-blankline.lua
+++ astronvim/lua/user/plugins/indent-blankline.lua
@@ -1,7 +1,21 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   opts = function(_, opts)
+    vim.opt.listchars:append("space: ")
+    vim.opt.listchars:append("eol: ")
     opts.show_trailing_blankline_indent = true
+    opts.show_current_context_start = true
+    opts.space_char_blankline = " "
+    opts.show_current_context = true
+
+    opts.char_highlight_list = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+      "IndentBlanklineIndent3",
+      "IndentBlanklineIndent4",
+      "IndentBlanklineIndent5",
+      "IndentBlanklineIndent6",
+    }
     return opts
   end
 }
--- astronvim/lua/user/plugins/nvim-treesitter.lua
+++ astronvim/lua/user/plugins/nvim-treesitter.lua
@@ -6,17 +6,6 @@ return {
       "git_config", "git_rebase", "gitcommit", "gitattributes", "awk",
       "jq", "jsdoc", "json", "json5", "markdown", "markdown_inline", "tsx", "yaml"
     }
-    opts.rainbow = {
-      enable = true,
-      -- list of languages you want to disable the plugin for
-      -- disable = { "jsx", "cpp" },
-      -- Which query to use for finding delimiters
-      query = 'rainbow-parens',
-      -- Highlight the entire buffer all at once
-      strategy = require 'ts-rainbow.strategy.global',
-      -- Do not enable for files with more than n lines
-      max_file_lines = 3000
-    }
     return opts
   end
 }
